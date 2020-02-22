ffa38ad0fe23523f56f45ef3e6baf04802ec572d
Mitchell-Johnson <mitchj9@uw.edu>
2020-02-21 19:57:56 -0800

    good progress

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-const Manager = require("./lib/Manager");
+const Manager = require("./lib/Manager.js");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inq = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-​
+
 const OUTPUT_DIR = path.resolve(__dirname, "output")
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
-const render = require("./lib/htmlRenderer");
+
+const HTMLrenderer = require("./lib/htmlRenderer");
+const render = new HTMLrenderer();
+
+const man = new Manager('bill', 1, 'bob@email.com', 5);
+console.log(man.getRole());
