f003b04e27905479895a1caa919f74110c66bd88
Mitchell-Johnson <mitchj9@uw.edu>
2020-02-22 13:28:27 -0800

    test

--- app.js
+++ app.js
@@ -11,5 +11,68 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const HTMLrenderer = require("./lib/htmlRenderer");
 const render = new HTMLrenderer();
 
-const man = new Manager('bill', 1, 'bob@email.com', 5);
-console.log(man.getRole());
+inq.prompt([
+    {
+        message: "How many Engineers are in your team?",
+        name: 'numEngineers'
+    },
+    {
+        message: "How many Interns?",
+        name: "numInterns"
+    }
+]).then( (answers) => {
+    getTeamData(answers.numEngineers, answers.numInterns);
+})
+
+function getTeamData(numEngineers, numInterns) {
+
+    inq.prompt([
+        {
+            message: "What is your name?",
+            name: "name"
+        },
+        {
+            message: "What is your email?",
+            name: "email"
+        },
+        {
+            message: "What is your office number?",
+            name: "office"
+        },
+    ]).then( (answers) => render.addTeamMember(answers, 'Manager'));
+
+    for(let i = 0; i < numEngineers; i++) {
+        inq.prompt([
+            {
+                message: "What is your name?",
+                name: "name"
+            },
+            {
+                message: "What is your email?",
+                name: "email"
+            },
+            {
+                message: "What is your github username?",
+                name: "username"
+            },
+        ]).then( (answers) => render.addTeamMember(answers, 'Engineer'));
+    }
+    for(let i = 0; i < numInterns; i++) {
+        inq.prompt([
+            {
+                message: "What is your name?",
+                name: "name"
+            },
+            {
+                message: "What is your email?",
+                name: "email"
+            },
+            {
+                message: "What school do you attend?",
+                name: "school"
+            },
+        ]).then( (answers) => render.addTeamMember(answers, 'Intern'));
+    }
+
+    render.createPage(outputPath);
+}
